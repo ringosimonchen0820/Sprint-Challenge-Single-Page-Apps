c3ed7c55abf917453780e8cf5b6c44ed33315536
ringosimonchen0820 <ringosimonchen0820@gmail.com>
2019-12-20 11:44:33 -0500

    add new img for home page, finished MVP

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,47 @@
+// import React from "react";
+
+// export default function WelcomePage() {
+//   return (
+//     <section className="welcome-page">
+//       <header>
+//         <h1>Welcome to the ultimate fan site!</h1>
+//         <img
+//           className="main-img"
+//           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+//           alt="rick"
+//         />
+//       </header>
+//     </section>
+//   );
+// }
 import React from "react";
+import styled from 'styled-components'
+
+const Welcome = styled.div`
+  max-width:1000px;
+  display:flex;
+  flex-direction:column;
+  justify-content:flex-start;
+  align-items:center;
+  margin:30px auto;
+  height:1000px;
+  font-family: 'Tauri', sans-serif;
+`
+
+
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
-      <header>
+      <Welcome>
         <h1>Welcome to the ultimate fan site!</h1>
+        <p>Nobody exists on purpose. Nobody belongs anywhere. We're all going to die. Come watch TV.</p>
         <img
           className="main-img"
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          alt="rick"
+          src="http://vignette4.wikia.nocookie.net/rickandmorty/images/c/c8/Rick_and_Morty_logo.png/revision/latest?cb=20160907114210"
+          alt="rick and morty"
         />
-      </header>
+      </Welcome>
     </section>
   );
 }
