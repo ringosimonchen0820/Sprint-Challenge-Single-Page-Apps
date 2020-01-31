b08c2ef7843cd66f94ca3c287781f543ddd78081
SimonChen <ringosimonchen0820@gmail.com>
2020-01-31 12:38:02 -0500

    finished MVP

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,7 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+
 import styled from 'styled-components';
+
+
+const FormInput = styled.form`
+  text-align:center;
+  padding:15px;
+`
+
+const Content = styled.div`
+  max-width:1000px;
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+  margin:30px auto;
+  font-family: 'Tauri', sans-serif;
+`
+
+const Title = styled.h3`
+  text-align:center;
+  color:white;
+  text-decoration:none;
+`
+const CharacterContent = styled.p`
+  text-align:center;
+  color:white;
+  text-decoration:none
+`
 
 export default function SearchForm(props) {
  
