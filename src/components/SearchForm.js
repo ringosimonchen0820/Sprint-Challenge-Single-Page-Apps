ae46751f05fb5ba2f76b50d8111b83dc9aecf515
SimonChen <ringosimonchen0820@gmail.com>
2020-01-31 12:07:24 -0500

    import react libraries

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import styled from 'styled-components';
 
 export default function SearchForm() {
  
