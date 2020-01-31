19831994c66bac87f1b34ad09f093c6d397e725b
SimonChen <ringosimonchen0820@gmail.com>
2020-01-31 11:56:16 -0500

    set up routing components

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ Demonstrate your understanding of this Sprint's concepts by answering the follow
 - [ ]  Explain benefit(s) using `client-side routing`?
 
 > Answer:
-
+Won’t refresh when using client-side routing. There are just some elements inside the application that will change. Because less data is processed, routing between views is generally faster. Also, Smooth transitions and animations between views are easier to implement.
 ## Project Set Up
 
 Follow these steps to set up and work on your project:
--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React from "react";
-import Header from "./components/Header.js";
+import { Route, Link} from 'react-router-dom'
+import styled from 'styled-components'
+import WelcomePage from './components/WelcomePage'
+import CharacterList from './components/CharacterList'
+import CharacterCard from './components/CharacterCard'
+import SearchForm from './components/SearchForm'
 
 
-export default function App() {
+const Main = styled.div`
+  background: linear-gradient(to left, #159957, #155799);
+  color: white;
+  height:100%;
+`
+
+const Nav = styled.nav` 
+  display: flex;
+  justify-content: center;
+  justify-content: space-around;
+  padding:30px;
+  font-size: 1.5rem;
+`
+
+export default function App(props) {
   return (
-    <main>
-      <Header />
-    </main>
+    <Main>
+  
+      <Nav>
+        <Link className="navLinks" to="/">Home</Link>
+        <Link className="navLinks" to="/searchcharacter">Search by Name </Link>
+        <Link className="navLinks" to="/character">Characters</Link>
+      </Nav>
+
+
+      <div>
+        <Route exact path="/" component={WelcomePage} />
+        <Route exact path="/character" component={CharacterList}/>
+        <Route path="/searchcharacter" component={SearchForm}/>
+        <Route path="/character/:id" render={props => <CharacterCard {...props} /> } />
+      </div>
+    </Main>
   );
 }
